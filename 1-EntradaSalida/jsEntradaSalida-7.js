d7661c887f756410358b151dfcd1107040be5001
nicolasmayans <nico.mayans@gmail.com>
2018-02-06 15:36:08 -0300

    Ejercicio 7
    suma resta div y mult

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,49 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numerouno;
+	var numerodos;
+	var suma;
+
+	numerouno = document.getElementById('numeroUno').value;	
+	numerodos = document.getElementById('numeroDos').value;
+	suma = (parseInt(numerouno)) + (parseInt(numerodos));	
+	alert("El resultado de la suma es " + suma);	
 }
 
 function restar()
 {
-	
+	var numerouno;
+	var numerodos;
+	var resta;
+
+	numerouno = document.getElementById('numeroUno').value;	
+	numerodos = document.getElementById('numeroDos').value;
+	resta = (parseInt(numerouno)) - (parseInt(numerodos));	
+	alert("El resultado de la resta es " + resta);
 }
 
 function multiplicar()
 { 
-	
+	var numerouno;
+	var numerodos;
+	var multi;
+
+	numerouno = document.getElementById('numeroUno').value;	
+	numerodos = document.getElementById('numeroDos').value;
+	multi = (parseInt(numerouno)) * (parseInt(numerodos));
+	alert("El resultado de la multiplicación es " + multi);
 }
 
 function dividir()
 {
-	
+	var numerouno;
+	var numerodos;
+	var division;
+
+	numerouno = document.getElementById('numeroUno').value;	
+	numerodos = document.getElementById('numeroDos').value;
+	division = (parseInt(numerouno)) / (parseInt(numerodos));	
+	alert("El resultado de la division es " + division);
 }
 
