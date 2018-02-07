0e05165d28dac3c7d33265276837fea02433ca72
nicolasmayans <nico.mayans@gmail.com>
2018-02-06 21:59:45 -0300

    Ejercicio tres
    entrada y salida de datos

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,22 @@
 function Mostrar()
 {
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	var suma;
+	var promedio;
 
+	preciouno = document.getElementById('precioUno').value;
+	preciodos = document.getElementById('precioDos').value;
+	preciotres = document.getElementById('precioTres').value;
+
+	preciouno = parseInt(preciouno);
+	preciodos = parseInt(preciodos);
+	preciotres = parseInt(preciotres);
+
+	suma = preciouno + preciodos + preciotres;
+	promedio = suma/3;
+
+	alert("La suma de los tres precios es " + suma);
+	alert("El promedio es " + promedio);
 }
