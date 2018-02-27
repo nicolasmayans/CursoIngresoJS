094f183276f6ebb35e0eb3c80a25b1e828bb4282
nicolasmayans <nico.mayans@gmail.com>
2018-02-26 21:55:51 -0300

    siete finalizar
    finalizar y testear siete

--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,41 @@
+// ingresar dos datos una letra y un número, los números van a estar entre -50 y +50, informar el promedio de los números
+//positivos, la cantidad de los números impares, la suma de todos los números cuya letra haya sido una vocal, el número
+//máximo y la letra con la cuál fue ingresado.
+
+//cantidad de impares numero %2 !=0
+// suma de letras vocal if letra =="a" || letra =="b" etc,
+
 function Mostrar()
 {
+	var letra;
+	var numero;
+	var promPosit;
+	var cantPosit;
+	numero=parseInt(numero);
+	promPosit=parseInt(promPosit);
+	cantPosit = 0;
+	promPosit = 0;
 
+
+	while(letra!=="no")
+	{
+		letra = prompt("Ingrese una letra");
+		while(!isNaN(letra))
+		{
+			letra = prompt("Ingrese una letra");
+		}
+		numero = prompt("Ingrese un numero entre -50 y 50");
+
+		//numero< -50 || numero > 50 || isNaN(numero)
+		if(numero>=0)
+		{
+			promPosit = promPosit + numero;
+			cantPosit = cantPosit + 1;
+			promPosit = promPosit / cantPosit;
+			
+		}
+
+		letra = prompt("¿Desea seguir cargando?");
+	}
+	alert("El promedio de los números positivos es " + promPosit);
 }
